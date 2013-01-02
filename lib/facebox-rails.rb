70caa4703a1fc45f1756b860b64571495d92a12b
Vlad Gorodetsky <v@gor.io>
2013-01-02 10:23:39 +0200

    Expose bundled version of Facebox

--- lib/facebox-rails.rb
+++ lib/facebox-rails.rb
@@ -1,1 +1,2 @@
 require 'facebox-rails/engine' if ::Rails.version >= '3.1'
+require 'facebox-rails/version'
--- lib/facebox-rails/version.rb
+++ lib/facebox-rails/version.rb
@@ -1,5 +1,6 @@
 module Facebox
   module Rails
     VERSION = "0.1.3"
+    FACEBOX_VERSION = "30d3f057c466c5a56e36fbdd0a40523a1c38617a"
   end
 end
